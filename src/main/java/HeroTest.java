19e934f68245727d329e1b18cffdb092e1e19621
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2021-03-23 19:13:06 +0200

    added parametres to class Hero

--- src/main/java/HeroTest.java
+++ src/main/java/HeroTest.java
@@ -1,22 +1,15 @@
-class HeroTest {
-    public static void main(String[] args) {
-        Hero hero = new Hero();
-
-        //Expect Paratrooper
-        System.out.println(hero.getName());
-
-        //Expect 100
-        System.out.println(hero.getHp());
-    }
-}
-
 class Hero {
     private String name;
     private int hp;
 
-    Hero() {
+    public Hero () {
         this.name = "Paratrooper";
         this.hp = 100;
+    }
+
+    public Hero (String name, int hp) {
+        this.name = name;
+        this.hp = hp;
     }
 
     public String getName() {
@@ -25,5 +18,21 @@ class Hero {
 
     public int getHp() {
         return hp;
+    }
+}
+
+class HeroTest {
+    public static void main(String[] args) {
+        //Expect Paratrooper
+        System.out.println(new Hero().getName());
+        //Expect 100
+        System.out.println(new Hero().getHp());
+
+        Hero hero = new Hero("Stranger", 50);
+
+        //Expect Stranger
+        System.out.println(hero.getName());
+        //Expect 50
+        System.out.println(hero.getHp());
     }
 }
