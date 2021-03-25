177a769259c7e24476f5809d2418c03def575f19
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2021-03-25 20:38:05 +0200

    added class QuarkeTrack

---
+++ src/main/java/QuarkeTrack.java
@@ -0,0 +1,58 @@
+class QuarkeTrack {
+    private int track;
+
+    public QuarkeTrack(int[] lines) {
+        int sumOfTracks = 0;
+        for (int i = 0; i < lines.length; i++) {
+            sumOfTracks += lines[i];
+        }
+        this.track = sumOfTracks;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        }
+        QuarkeTrack other = (QuarkeTrack) obj;
+        if (track != other.track)  {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + track;
+        return result;
+    }
+}
+
+class QuarkeTrackTest {
+    public static void main(String[] args) {
+        int[] track1Data = {1, 3, 5};
+        int[] track2Data = {3, 5, 4};
+        int[] track3Data = {1, 5, 3};
+
+        QuarkeTrack track1 = new QuarkeTrack(track1Data);
+        QuarkeTrack track2 = new QuarkeTrack(track2Data);
+        QuarkeTrack track3 = new QuarkeTrack(track3Data);
+
+        //false
+        System.out.println(track1.equals(track2));
+
+        //true
+        System.out.println(track1.equals(track3));
+
+        //Can be true or false
+        System.out.println(track1.hashCode() == track2.hashCode());
+
+        //true
+        System.out.println(track1.hashCode() == track3.hashCode());
+    }
+}
