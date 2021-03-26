921632fb2af4dd4bad4882268ab3d1df7e9f0f66
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2021-03-26 20:46:30 +0200

    rebuild class Level with inner class Point

--- src/main/java/Level.java
+++ src/main/java/Level.java
@@ -1,19 +1,48 @@
 class Level {
-    private String name;
+    private String levelName;
+    private Point[] points;
 
-    public Level(String name) {
-        this.name = name;
+    public Level(String levelName, Point[] points) {
+        this.levelName = levelName;
+        this.points = points;
+    }
+
+    static class Point {
+        private int x;
+        private int y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int getX() {
+            return x;
+        }
+
+        public int getY() {
+            return y;
+        }
     }
 
     @Override
     public String toString() {
-        return "Quarke level, name is " + this.name;
+        return "Quarke level, name is " + this.levelName + ", point count is " + points.length;
     }
 }
 
 class LevelTest {
     public static void main(String[] args) {
+        Level.Point p1 = new Level.Point(3, 5);
+        Level.Point p2 = new Level.Point(10, 100);
+        Level.Point p3 = new Level.Point(50, 40);
+
+        Level.Point[] points = {p1, p2, p3};
+
         //Quarke level, name is Test
-        System.out.println(new Level("Test"));
+        //System.out.println(new Level("Test"));
+
+        //Quarke level, name is Test, point count is 3
+        System.out.println(new Level("Test", points));
     }
 }
